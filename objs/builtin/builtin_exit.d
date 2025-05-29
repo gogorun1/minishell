1318759697b40e1025570ec8c6d640c8dcf43d9f
Luxuan Cao <lcao@f2r10s16.clusters.42paris.fr>
2025-05-29 15:33:36 +0200

    add execution to main.c

--- objs/builtin/builtin_exit.d
+++
@@ -1,3 +0,0 @@
-objs/builtin/builtin_exit.o: srcs/builtin/builtin_exit.c \
-  includes/minishell.h libft/includes/libft.h libft/includes/ft_printf.h \
-  libft/includes/get_next_line.h libft/includes/get_next_line_bonus.h
