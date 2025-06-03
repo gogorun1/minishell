ab7e01b1ff5b7cc73dc1f0792e4a032868e57a28
Luxuan Cao <lcao@f1r7s22.clusters.42paris.fr>
2025-06-03 16:58:45 +0200

    cd error passed, but export needed to be test

--- srcs/builtin/builtin_cd.c
+++ srcs/builtin/builtin_cd.c
@@ -6,7 +6,7 @@
 /*   By: lcao <lcao@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 16:43:40 by lcao              #+#    #+#             */
-/*   Updated: 2025/06/03 16:43:55 by lcao             ###   ########.fr       */
+/*   Updated: 2025/06/03 16:54:20 by lcao             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@ int	builtin_cd(char **args, t_shell *shell)
 	if (args[1] && args[2]) {
 		ft_fprintf(2, "minishell: cd: too many arguments\n");
 		if (shell) shell->last_exit_status = 1;
-		return 1;
+		return (1);
 	}
 	if (!args[1]) {
-		path = getenv("HOME");
+		path = my_getenv("HOME", shell->env_list);
 		if (!path) {
 			ft_fprintf(2, "minishell: cd: HOME not set\n");
 			if (shell) shell->last_exit_status = 1;
-			return 1;
+			return (1);
 		}
 	} else {
 		path = args[1];
@@ -34,9 +34,9 @@ int	builtin_cd(char **args, t_shell *shell)
 	if (chdir(path) != 0) {
 		ft_fprintf(2, "minishell: cd: %s: %s\n", path, strerror(errno));
 		if (shell) shell->last_exit_status = 1;
-		return 1;
+		return (1);
 	}
 	if (shell) shell->last_exit_status = 0;
-	return 0;
+	return (0);
 }
 
