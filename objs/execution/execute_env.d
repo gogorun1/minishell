0b5032b6fe67c601fd98492dc8a94a8530a7eedf
Wenhan Ding <wding@f1r2s23.clusters.42paris.fr>
2025-06-13 16:35:48 +0200

    signal seems good, heredoc with issue

--- objs/execution/execute_env.d
+++
@@ -1,3 +0,0 @@
-objs/execution/execute_env.o: srcs/execution/execute_env.c \
-  includes/minishell.h libft/includes/libft.h libft/includes/ft_printf.h \
-  libft/includes/get_next_line.h libft/includes/get_next_line_bonus.h
