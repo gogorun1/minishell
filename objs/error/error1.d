bc2dcfa7d9480e631fc2e736cb8b19e56b37c863
Luxuan Cao <lcao@f6r10s9.clusters.42paris.fr>
2025-06-14 19:45:18 +0200

    builtin norm fixed

--- objs/error/error1.d
+++
@@ -1,3 +0,0 @@
-objs/error/error1.o: srcs/error/error1.c includes/minishell.h \
-  libft/includes/libft.h libft/includes/ft_printf.h \
-  libft/includes/get_next_line.h libft/includes/get_next_line_bonus.h
