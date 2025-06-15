8f46bb5919effb424eac34c77bf82563e35e511b
Wenhan Ding <wding@f1br4s7.clusters.42paris.fr>
2025-06-15 15:27:55 +0200

    ctrl-c in heredoc\child\parent\recursive fixed, ctrl-d in heredoc still wired

--- objs/error/utility_error.d
+++
@@ -1,3 +0,0 @@
-objs/error/utility_error.o: srcs/error/utility_error.c \
-  includes/minishell.h libft/includes/libft.h libft/includes/ft_printf.h \
-  libft/includes/get_next_line.h libft/includes/get_next_line_bonus.h
