bb1e63eaf443578176de29787294d67d9690534f
Luxuan Cao <lcao@f1br5s6.clusters.42paris.fr>
2025-06-15 23:52:38 +0200

    env free fixed

--- objs/execution/heredoc_utils.d
+++
@@ -1,3 +0,0 @@
-objs/execution/heredoc_utils.o: srcs/execution/heredoc_utils.c \
-  includes/minishell.h libft/includes/libft.h libft/includes/ft_printf.h \
-  libft/includes/get_next_line.h libft/includes/get_next_line_bonus.h
